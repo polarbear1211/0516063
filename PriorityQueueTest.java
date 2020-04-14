2c9b582ec3e9b5f7ea62fc0bf7c4003e208f2385
polarbear1211 <33109379+polarbear1211@users.noreply.github.com>
2020-04-14 22:38:20 +0800

    Update PriorityQueueTest.java

--- PriorityQueueTest.java
+++ PriorityQueueTest.java
@@ -1,4 +1,4 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
